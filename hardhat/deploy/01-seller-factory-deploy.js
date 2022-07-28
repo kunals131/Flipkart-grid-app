9b1a68c13b4f6cba9fc241b64e897e428256cd84
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-07-28 13:11:51 +0530

    added Chainlink keepers to keep track of expiry time

---
+++ hardhat/deploy/01-seller-factory-deploy.js
@@ -0,0 +1,26 @@
+const {network} = require('hardhat');
+const { developmentChains } = require('../helper-hardhat-config');
+const { verify } = require('../utils/verify');
+
+module.exports = async function({getNamedAccounts,deployments}) {
+    const {deploy,log} = deployments;
+    const {deployer} = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    const args = [];
+    console.log('deploying...');
+    const sellerContract = await deploy("Seller",{
+        from : deployer,
+        args,
+        log : true,
+        waitConfirmations : network.config.blockConfirmations || 1
+    });
+
+    log("-----------------------------");
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log('Verifying....');
+        await verify(sellerContract.address,args);  
+    }
+    log("----------------------");
+
+}
+
--- hardhat/deploy/02-mint.js
+++ hardhat/deploy/02-mint.js
@@ -15,6 +15,7 @@ const metaDataTemplate = {
 module.exports = async({getNamedAccounts,deployments})=>{
     const {deployer} = await getNamedAccounts();
     const chainId = network.config.chainId;
+    const {log} = deployments;
 
     let tokenUris = [
         'ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4',
@@ -22,19 +23,22 @@ module.exports = async({getNamedAccounts,deployments})=>{
         'ipfs://QmbvRqKSQxmNEHRY9o8Tzwvf1BoQb433kfkr5SLpKDgyiz'
       ]
 
+      
     //create-Seller (Flipkart's job)
     const sellerContract = await ethers.getContract("Seller", deployer);
     console.log(`Seller contract created with address ${sellerContract.address}`)
     const sellerTxt = await sellerContract.createSeller(deployer, "Ins", "INS", {gasLimit : "5000000"});
+    
     // console.log(`Seller created with hash ${sellerTxt.hash}`);
     let contractAddress = '';
     const sellersWarrantyContractAddress = await sellerContract.getWarrantyContract({gasLimit : "5000000"});
     await new Promise((resolve,reject)=>{
         setTimeout(resolve, 600000) // 5 minute timeout time
 
-        sellerContract.once('SellerCreated', (e)=>{
-            console.log(sellersWarrantyContractAddress);
+        sellerContract.once('SellerCreated', async(e)=>{
+            // console.log(sellersWarrantyContractAddress);
             contractAddress = e;
+         
             console.log(e, 'Contract');
             console.log('Seller Created!');
             resolve();
@@ -50,11 +54,12 @@ module.exports = async({getNamedAccounts,deployments})=>{
     console.log(`Customer bought with order Id ${123}`);
 
 
-    const mintTx = await sellerWarrantyContract.mintWarrantyNFT(deployer,'123',tokenUris[0],tokenUris[1],'12 Dec 2022', {gasLimit : "5000000"});
+    const mintTx = await sellerWarrantyContract.mintWarrantyNFT(deployer,'123',tokenUris[0],tokenUris[1],'400', {gasLimit : "5000000"});
     await new Promise((resolve,reject)=>{
         setTimeout(resolve, 600000) // 5 minute timeout time
         sellerWarrantyContract.once('NFTMinted', (e)=>{
-            console.log(e, 'NFT Created');
+            console.log(e);
+            console.log('NFT Created');
             console.log('NFT CREATED!!');
             resolve();
         })
