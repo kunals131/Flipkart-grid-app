e394364180291c0636b2c8107d02976063bca355
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-07-28 17:46:06 +0530

    Added Filters component, Products Page & header

---
+++ components/Products/ProductsContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductsContainer = () => {
+  return (
+    <div className='bg-bgPrimary-700 px-6 py-3'>
+        <div className='font-[500] text-textPrimary text-2xl'>All Products <span className='ml-2 text-sm'>(Showing 64 products out of 500)</span></div>
+        <hr  className='border-gray-500'/>
+        <div className='mt-8'>
+            {/* Products */}
+            
+        </div>
+    </div>
+  )
+}
+
+export default ProductsContainer
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import FilterComponent from "../components/FilterBar/FilterComponent";
+import Filters from "../components/FilterBar/Filters";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="grid grid-cols-[0.22fr_0.8fr] gap-x-10 p-4">
+          <Filters/>
+          
+      </div>
     </div>
   )
 }
