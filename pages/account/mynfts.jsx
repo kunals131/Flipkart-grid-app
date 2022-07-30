c5db02dc5576397889bbd48dd6fa37f91e604661
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-07-30 18:28:44 +0530

    added NFT Page

--- pages/account/mynfts.jsx
+++ pages/account/mynfts.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AccountLayout from '../../components/AccountLayout'
+import {useMoralis} from 'react-moralis';
+import {useWeb3Contract} from 'react-moralis'
+import { verifyAuthentication } from '../../utils/verifyAuth';
+import WarrantyABI from '../../constants/WarrantyNFTABI.json';
+export const getServerSideProps = async(ctx) => {
+  const auth = verifyAuthentication(ctx.req);
+  if (auth.state) {
+    try {
+    return {props : {user : auth.decodedData.user}};
+    }catch(err) {
+      console.log(err);
+      return {notFound : true};
+    }
+  }
+  return {
+    redirect : {
+      destination : '/auth'
+    }
+  }
+};
 
-const MyNFTs = () => {
+const MyNFTs = ({user}) => {
   const [loading,setLoading] = useState(false);
-  
+  const {account,chainId,isWeb3Enabled} = useMoralis();
+  const [message,setMessage] = useState('');
+  useEffect(()=>{
+    if (isWeb3Enabled) {
+      setMessage(`Showing NFTs for the account ${account}`);
+    }
+  }, [isWeb3Enabled])
+
+  // const {runContractFunction : getCustomersTokens} = useWeb3Contract({
+  //   abi : WarrantyABI,
+  //   contractAddress : 
+  // });
   return (
     <AccountLayout>
-      <div className='text-2xl font-semibold p-5'>Warranty NFTs</div>
+      <div className='p-5'>
+      <div className='text-2xl font-semibold'>Warranty NFTs</div>
+      <p className='text-gray-600 mt-1 text-sm'>{message}</p>
+      </div>
+
+
     </AccountLayout>
   )
 }
